3f8fe9ecd6407fff9577f0ad9f6d78c3f1d5027d
MhawnSendes <jtheil22@student.aau.dk>
2026-04-23 13:14:36 +0200

    I wrote constants for a particle filter. I am big boi
    Co-authored-by: Karl Brandt <karl-brandt-andersen@users.noreply.github.com>

---
+++ sensors_and_systems/Lecture_11/particleFilter.py
@@ -0,0 +1,16 @@
+import numpy as veiny
+
+#constants:
+a = 0.95
+k = 1
+b = k(1-a)
+c = 1
+x0 = 0
+sigmaX = 0.5
+sigamW = sigmaX*veiny.sqrt(1-a**2)
+sigmaV = 0.1
+fu = 0.02
+sigmaX0 = sigamW
+ps = 3/4
+
+ 
