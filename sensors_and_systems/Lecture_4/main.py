ed0a0aedfa5d1c18f7026690c103adac3c3521d7
MhawnSendes <jtheil22@student.aau.dk>
2026-05-29 09:34:21 +0200

    Måske man skulle comitte det man lavede igår
    Co-authored-by: Karl Brandt <karl-brandt-andersen@users.noreply.github.com>
    Co-authored-by: Mikkelnn <Mikkelnn@users.noreply.github.com>
    Co-authored-by: Vinicius Hodal <Handburgerhand@users.noreply.github.com>
    Co-authored-by: VIGNESH SHAILESH BHAT <Vigs-B@users.noreply.github.com>

--- sensors_and_systems/Lecture_4/main.py
+++ sensors_and_systems/Lecture_4/main.py
@@ -1,4 +1,29 @@
 import numpy as np
 
+rand = np.random.default_rng()
+steps = 100
+
+w = rand.standard_normal(steps)
+v = rand.standard_normal(steps)
+
+def ut_alg(x, P, alpha = 1, kappa = 2, beta = 0):
+    x_i = [0]
+    w_i = [0]
+
+    n = x.size
+    lambda_ = alpha**2 * (n + kappa) - n
+    k = np.sqrt(n + lambda_)
+
+    l, u = np.linalg.eig(P)
+    for i in range(l+1):
+        if i == 0:
+            x_i.append(np.mean(x))
+        elif 1 <= i <= n:
+            x_i.append(np.mean(x) + k*u[i]*np.sqrt(l[i]))
+        elif n+1 <= i <= 2*n:
+            x_i.append(np.mean(x) - k*u[i]*np.sqrt(l[i]))
+
+
 if __name__ == "__main__":
-    pass
+    for _ in range(steps):
+        pass
